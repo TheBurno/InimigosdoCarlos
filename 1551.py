b85fb05fc01a57194318447aebd79bc70e699a2a
Bruno Cardoso de Oliveira <119974445+TheBurno@users.noreply.github.com>
2023-09-01 11:54:08 -0400

    danilo

--- 1551.py
+++ 1551.py
@@ -1,5 +1,9 @@
-n = int(input())
+N = int(input())
 
-for _ in range(n):
-    letras = list()
-    letras = 
+for _ in range(N):
+    letras = []
+    frase = input()
+    for letra in frase:
+        if letra not in letras:
+            letra.append(letras)
+        
