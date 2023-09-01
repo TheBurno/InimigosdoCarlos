f0ca99702c891d0c9fab2d3482dc16e4a46f4ac4
Bruno Cardoso de Oliveira <119974445+TheBurno@users.noreply.github.com>
2023-09-01 11:54:28 -0400

    danilo

--- 1551.py
+++ 1551.py
@@ -4,6 +4,12 @@ for _ in range(N):
     letras = []
     frase = input()
     for letra in frase:
-        if letra not in letras:
-            letra.append(letras)
+        if (letra.isalpha()) and (letra not in letras):
+            letras.append(letra)
+    if len(letras) == 26:
+        print("frase completa")
+    elif len(letras) >= 13:
+        print("frase quase completa")
+    else:
+        print("frase mal elaborada")
         
